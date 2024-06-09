53cf13aacc5891c50c1ebfdc4f7aa311986ba545
SOBOJBANGLA <164489940+SOBOJBANGLA@users.noreply.github.com>
2024-06-10 00:28:00 +0600

    night

--- 9jun2024/php_syntex.php
+++ 9jun2024/php_syntex.php
@@ -1,17 +1,17 @@
 <?php
-//full syntax
- echo "Hello world <br>";
+// full syntax
+echo "Hello World <br>";
+
+?>
+
+<?php
+// short open Tag
+echo "Hello World with short tag <br>";
+?>
+
+<?= "Hello World with short circuit<br>"
+// short circuit
 
 ?>
  
-<?
-// open short tag
- echo "Hello world tag<br>";
 
- ?>
-
-<?= "Hello world tag circuit";
-// short circuit
- 
-
- ?>
