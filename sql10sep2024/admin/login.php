911b496f56c5958778cc3628c0b1938205a9c4bf
Abdullah <abuhmdabdullah72@gmail.com>
2024-09-10 13:24:04 +0600

    10ssss

---
+++ sql10sep2024/admin/login.php
@@ -0,0 +1,49 @@
+<?php
+require_once "dbconfig.php";
+session_start();
+
+if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password = md5($password);
+    $usertype = $_POST['usertype'];
+
+}
+
+if($usertype==1){
+$sql = $con->query("SELECT * FROM admin WHERE email = '$email' AND password = '$password'");
+ if($sql->num_rows>0){
+    //session
+    $_SESSION['email']= $email;
+    $_SESSION['utype']= $usertype;
+
+    //Redirect
+header("location : admin_dashboard.php");
+ }
+}
+
+if($usertype==2){
+    $sql = $con->query("SELECT * FROM doctors WHERE docEmail = '$email' AND password = '$password'");
+ if($sql->num_rows>0){
+    //session
+    $_SESSION['email']= $email;
+    $_SESSION['utype']= $usertype;
+
+    //Redirect
+    header("location : doctors_dashboard.php");
+ }
+}
+
+if($usertype==3){
+    $sql = $con->query("SELECT * FROM users WHERE email = '$email' AND password = '$password'");
+ if($sql->num_rows>0){
+    //session
+    $_SESSION['email']= $email;
+    $_SESSION['utype']= $usertype;
+
+    //Redirect
+    header("location : users_dashboard.php");
+ }
+}
+
+?>
